4ac0ab730a1a14081c1c4981e60ab46f2ec8e0a7
LisitskayaElizaveta <kapka2038@gmail.com>
2024-12-05 17:31:52 +0300

    Add files via upload

---
+++ Type6.java
@@ -0,0 +1,13 @@
+
+public class Type6 {
+    public static void main(String args[]) {
+        int i = 123456;
+        byte b = (byte) i;
+        System.out.println(b);
+
+        double d = 1.2345678901234d;
+        float f = (float) d;
+        System.out.println(f);
+        
+    }
+}
