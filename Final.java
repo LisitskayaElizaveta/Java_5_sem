60a3af7da528fbf6ae8b06e25adfa5d3079be7bc
LisitskayaElizaveta <kapka2038@gmail.com>
2024-12-07 20:31:47 +0300

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,29 @@
+public class Final {
+    //1 в блоке инициализации
+    public final int a;
+    {
+        a=10;
+    }
+
+    //2 инициализация при объявлении
+    //public final int a = 1234;
+
+    //3 присвоение в конструкоре
+    /*
+    public final int a;
+    public Final(int value) {
+        this.a = value;
+    }
+    */
+
+    //1.5 если статическая
+    /*
+    public static final int a;
+
+    static {
+        a = 1234;
+    }
+     */
+
+
+}
